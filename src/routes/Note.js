51dc8ba0c2cf0e5931c34d9a6cc2b654933b5982
Enes Sefa Erdoğan <70656241+enessefaerdogan@users.noreply.github.com>
2023-05-06 01:26:46 +0300

    Update Note.js

--- src/routes/Note.js
+++ src/routes/Note.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Note = require('./../models/Note');
 
 router.get("/list/:userid", async function(req, res) {
-    var notes = await Note.find({ userid: req.body.userid });
+    var notes = await Note.find({ userid: req.params.userid });
     res.json(notes);
 });
 
